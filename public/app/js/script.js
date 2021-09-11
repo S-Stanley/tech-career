e432699345c3e33278cce86097acd66dd64edfd5
Stanley Serbin <stanleyserbin@gmail.com>
2021-09-11 15:23:09 +0200

    ADD hero section and hamberger menu

--- public/app/js/script.js
+++ public/app/js/script.js
@@ -1,1 +1,12 @@
 console.log("hello word");
+
+function show_or_hide_menu_mobile(){
+	menu_mobile = document.getElementById("menu_mobile");
+	if (getComputedStyle(menu_mobile).visibility == "hidden"){
+		menu_mobile.style.visibility = "visible";
+		menu_mobile.style.position = "relative"
+		return ;
+	}
+	menu_mobile.style.visibility = "hidden";
+	menu_mobile.style.position = "absolute"
+}
